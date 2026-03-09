cdb1bd76a7f5fd5f3b9f243e0f5272d4eb4b739a
musteryasm-browserstack <shivam.m@browserstack.com>
2026-03-09 10:10:55 +0530

    Change SLACK_CHANNEL to a new value

--- daily_report.py
+++ daily_report.py
@@ -24,7 +24,7 @@ BS_HEADERS = {
 #"C06T7FZ0BFZ" #lcnc qa 
 #C09SV14UWBZ - shivam-testing
 
-SLACK_CHANNEL = "C09SV14UWBZ" #lcnc qa 
+SLACK_CHANNEL = "C06T7FZ0BFZ" #lcnc qa 
 QA_OPS_GROUP_ID = "S07L05V67B7" 
 
 IST = pytz.timezone("Asia/Kolkata")
